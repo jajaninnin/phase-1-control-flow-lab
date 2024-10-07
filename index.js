825d0ed7bd13a41ff8f5782488159cfaf60978a1
janine pamintuan <jaja.ninnin@gmail.com>
2024-10-07 12:06:09 -0400

    control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let greeting
+  if (ride <= 400) {
+    return greeting = "This one is on me!";
+  } else if (ride <= 2000 && ride > 400) {
+    return greeting = "That will be twenty bucks.";
+  } else if (ride > 2000 && ride < 2500) {
+    return greeting = "I will gladly take your thirty bucks."
+  } else if (ride >= 2500) {
+    return greeting = "No can do."
+  }
+};
+
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let message;
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+  }
 }
